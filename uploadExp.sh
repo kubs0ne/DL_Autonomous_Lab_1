de92484e4e1794288a589d9f3bdf11c0d0882648
Bennetz <bernhard.boeckenhoff@edu.udo>
2022-03-17 13:01:17 +0100

    fixed the upload script

--- uploadExp.sh
+++ uploadExp.sh
@@ -1,14 +1,4 @@
 
-#SSH into the cluster to create new directory
-ssh nct01029@plogin1.bsc.es
-cd AutoLab1/Experiments
-mkdir 1
-exit
+scp -r Experiments/1 nct01029@dt01.bsc.es:/home/nct01/nct01029/AutoLab1/Experiments #Upload the current experiment
+ssh nct01029@plogin1.bsc.es 'bash -s < AutoLab1/Experiments/1/ex1.sh' #Run the local Bash script
 
-#Upload experiment
-#scp Experiments/1 nct01029@dt01.bsc.es:/home/nct01/nct01029/AutoLab1/Experiments/1
-
-#SSH into the cluster
-#ssh nct01029@plogin1.bsc.es
-#sh ex1.sh
-#exit
