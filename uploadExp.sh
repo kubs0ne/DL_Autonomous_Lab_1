ab02cb6c4a6134b8b0e1459f5b8e382e604110b8
Bennetz <bernhard.boeckenhoff@edu.udo>
2022-03-17 12:24:34 +0100

    upload script

--- uploadExp.sh
+++ uploadExp.sh
@@ -1,3 +1,14 @@
 
-scp Experiments/1 nct01029@dt01.bsc.es:/home/nct01/nct01029/AutoLab1/Exp1
+#SSH into the cluster to create new directory
+ssh nct01029@plogin1.bsc.es
+cd AutoLab1/Experiments
+mkdir 1
+exit
 
+#Upload experiment
+#scp Experiments/1 nct01029@dt01.bsc.es:/home/nct01/nct01029/AutoLab1/Experiments/1
+
+#SSH into the cluster
+#ssh nct01029@plogin1.bsc.es
+#sh ex1.sh
+#exit
