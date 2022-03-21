47fbaa7f3d042316517e440d63340b1857d9af89
Jakub Glowacz <kuba.glowacz@gmail.com>
2022-03-21 12:19:39 +0100

    corrected sh script

--- Experiments/1/ex1.sh
+++ Experiments/1/ex1.sh
@@ -4,11 +4,11 @@
 
 #SBATCH --qos=debug
 
-#SBATCH --workdir=/home/nct01/nct01029/AutoLab1/Experiments/1.
+#SBATCH --workdir=.
 
-#SBATCH --output=MAMeEx1_%j.out
+#SBATCH --output=/home/nct01/nct01036/AutoLab1/Experiments/1/MAMeEx1_%j.out
 
-#SBATCH --error=MAMeEx1_%j.err
+#SBATCH --error=/home/nct01/nct01036/AutoLab1/Experiments/1/MAMeEx1_%j.err
 
 #SBATCH --cpus-per-task=40
 
@@ -18,4 +18,4 @@
 
 module purge; module load gcc/8.3.0 ffmpeg/4.2.1 cuda/10.2 cudnn/7.6.4 nccl/2.4.8 tensorrt/6.0.1 openmpi/4.0.1 atlas/3.10.3 scalapack/2.0.2 fftw/3.3.8 szip/2.1.1 opencv/4.1.1 python/3.7.4_ML
 
-python ex1.py
+python /home/nct01/nct01036/AutoLab1/Experiments/1/ex1.py
