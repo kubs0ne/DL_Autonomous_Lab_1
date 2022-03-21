54fc53b82fc9cabaa262552f7dcdef10e3207bc1
Bennetz <bernhard.boeckenhoff@edu.udo>
2022-03-21 13:01:28 +0100

    fix model evaluator

--- ModelEvaluator.py
+++ ModelEvaluator.py
@@ -18,20 +18,16 @@ def evaluate_model(model, history, eval_gen ):
     # Evaluate the model
     eval_gen.reset()
     score = model.evaluate(eval_gen, verbose=0)
-    print('\nLoss:', score[0])
-    print('Accuracy:', score[1])
+    print('test loss:', score[0])
+    print('test accuracy:', score[1])
+
     # Confusion Matrix (validation subset)
     eval_gen.reset()
     pred = model.predict(eval_gen, verbose=0)
-
-    # Assign most probable label
-    predicted_class_indices = np.argmax(pred, axis=1)
-
-    # Get class labels
+    predicted_class_indices = np.argmax(pred, axis=1)    # Assign most probable label
     labels = (eval_gen.class_indices)
     target_names = labels.keys()
 
-    # Plot statistics
     print(classification_report(eval_gen.classes, predicted_class_indices, target_names=target_names))
 
     cf_matrix = confusion_matrix(np.array(eval_gen.classes), predicted_class_indices)
@@ -39,16 +35,13 @@ def evaluate_model(model, history, eval_gen ):
     sns.heatmap(cf_matrix, annot=True, cmap='PuRd', cbar=False, square=True, xticklabels=target_names,
                 yticklabels=target_names)
     plt.show()
-    plt.savefig('ex1.pdf')
+    plt.savefig('confusion_matrix.pdf')
+    plt.close()
 
-    ###########Added to the funciton
 
-    #Evaluate the model with test set
-    score = model.evaluate(eval_gen, verbose=0)
-    print('test loss:', score[0])
-    print('test accuracy:', score[1])
     ##Store Plots
     matplotlib.use('Agg')
+
     #Accuracy plot
     plt.plot(history.history['accuracy'])
     plt.plot(history.history['val_accuracy'])
@@ -56,8 +49,9 @@ def evaluate_model(model, history, eval_gen ):
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train','val'], loc='upper left')
-    plt.savefig('mnist_fnn_accuracy.pdf')
+    plt.savefig('accuracy.pdf')
     plt.close()
+
     #Loss plot
     plt.plot(history.history['loss'])
     plt.plot(history.history['val_loss'])
@@ -65,6 +59,6 @@ def evaluate_model(model, history, eval_gen ):
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train','val'], loc='upper left')
-    plt.savefig('mnist_fnn_loss.pdf')
+    plt.savefig('loss.pdf')
 
 
