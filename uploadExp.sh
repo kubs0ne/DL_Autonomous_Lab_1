aeea6e682a132c4c9c79422d6f410e7d863e0900
Bennetz <bernhard.boeckenhoff@edu.udo>
2022-03-24 12:41:09 +0100

    exp9

--- uploadExp.sh
+++ uploadExp.sh
@@ -1,4 +1,4 @@
 
-scp -r Experiments/1 nct01029@dt01.bsc.es:/home/nct01/nct01029/AutoLab1/Experiments #Upload the current experiment
-ssh nct01029@plogin1.bsc.es 'sbatch AutoLab1/Experiments/1/ex1.sh' #Submit sbatch 
+scp -r Experiments/9 nct01029@dt01.bsc.es:/home/nct01/nct01029/AutoLab1/Experiments #Upload the current experiment
+ssh nct01029@plogin1.bsc.es 'sbatch AutoLab1/Experiments/9/ex9.sh' #Submit sbatch 
 
